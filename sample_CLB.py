00ffa27957213288b8ece7deeb662f6e7d7cbed3
chansa19 <chanson19@gmail.com>
2015-12-17 20:19:51 -0500

    1st file
    committing to the 1st file

---
+++ sample_CLB.py
@@ -0,0 +1,10 @@
+print "hello world"
+name_of_student = "Jess"
+print name_of_student
+
+statement = "hello {0}, it's nice to meet you".format(name_of_student)
+print statement
+
+color = raw_input("whats your favorite color?")
+response = "well {0}, {1} is a great color, nice to meet you".format(name_of_student, color)
+print response
